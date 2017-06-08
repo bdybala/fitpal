712f7745ffb1299f66c76063805e337767906d53
Bartosz Dybała <dybala.bartosz@gmail.com>
2017-06-08 19:22:34 +0200

    no security

--- src/main/java/com/example/fitpal/config/Constants.java
+++ src/main/java/com/example/fitpal/config/Constants.java
@@ -2,6 +2,6 @@ package com.example.fitpal.config;
 
 public class Constants {
     public static final String DB_PREFIX = "myfitpal_";
-    public static final String ROLE_USER = "USER";
-    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
 }
